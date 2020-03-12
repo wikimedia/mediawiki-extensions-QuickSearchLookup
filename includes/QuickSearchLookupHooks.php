15761a70990e4aa6faa039ec977a26f6693d45ec
zoranzoki21 <zorandori4444@gmail.com>
2020-03-12 15:37:31 +0100

    Fix errors/warnings found by phpcs in QuickSearchLookupHooks.php
    Change-Id: Iffd153bd5865c498a246f943056a4459859fd8aa

--- includes/QuickSearchLookupHooks.php
+++ includes/QuickSearchLookupHooks.php
@@ -4,10 +4,13 @@ class QuickSearchLookupHooks {
 	/**
 	 * If there isn't a first title already, set it here.
 	 *
-	 * @param Title $title Title object of the list item
-	 * @param String $text Text to use for the link
+	 * @param Title &$title Title object of the list item
+	 * @param String &$text Text to use for the link
+	 * @param Result $result
+	 * @param Terms $terms
+	 * @param Page $page
 	 */
-	public static function onShowSearchHitTitle( &$title, &$text, $result, $terms, $pag ) {
+	public static function onShowSearchHitTitle( &$title, &$text, $result, $terms, $page ) {
 		$qsl = QuickSearchLookup::getMain();
 		if ( $qsl->needsFirstResult() ) {
 			$qsl->setFirstResult( $title );
@@ -17,9 +20,9 @@ class QuickSearchLookupHooks {
 	/**
 	 * If there isn't a first title already, set it here.
 	 *
-	 * @param SpecialSearch $special
+	 * @param SpecialSearch $page
 	 * @param String $profile current search profile
-	 * @param SearchEngine the search engine
+	 * @param SearchEngine $searchEngine the search engine
 	 */
 	public static function onSpecialSearchSetupEngine( SpecialSearch $page, $profile, $searchEngine ) {
 		$qsl = QuickSearchLookup::getMain();
